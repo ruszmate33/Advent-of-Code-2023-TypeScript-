a6e25cf9b1a3a9df06dce3bb08cb7e801ae9ac78
Mate Rusz <ruszmate33@gmail.com>
2024-02-05 07:26:46 +0100

    day2 part1 in TS solved

--- src/day2.ts
+++ src/day2.ts
@@ -6,7 +6,11 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
-const possibleCubes = { red: 12, green: 13, blue: 14 };
+interface colorNumColors {
+  [key: string]: number;
+}
+
+const possibleCubes: colorNumColors = { red: 12, green: 13, blue: 14 };
 
 // part1 puzzle input solution 2085
 // testInput with part1 is 8
@@ -25,61 +29,51 @@ function processGameSets(line: string): string[][] {
   return gameSets;
 }
 
-// function allPossibleDigitsFromLine(
-//   line: string,
-//   includeAlphaDigits: boolean
-// ): string {
-//   let digitsOnLine = "";
-//   for (let j = 0; j < line.length; j++) {
-//     let char = line[j];
-//     const parsed = parseInt(char);
-//     if (!isNaN(parsed)) {
-//       // console.log(`${line} ${parsed}`)
-//       digitsOnLine += parsed.toString();
-//     }
-//     if (includeAlphaDigits) {
-//       for (const [index, alphaDigit] of alphabeticDigits.entries()) {
-//         if (line.slice(j).startsWith(alphaDigit)) {
-//           digitsOnLine = (index + 1).toString();
-//           break
-//         }
-//       }
-//     }
-//   }
-//   return digitsOnLine;
-// }
+function validDraw(
+  drawNum: number,
+  drawColor: string,
+  possibleCubes: colorNumColors
+): boolean {
+  if (drawNum > possibleCubes[drawColor]) {
+    return false;
+  }
+  return true;
+}
 
-// function numberFromFirstAndLastDigit(digits: string): number {
-//   return parseInt(digits.charAt(0).concat(digits.slice(-1)));
-// }
+function sumOfNums(nums: number[]): number {
+  return nums.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue;
+  }, 0);
+}
 
 function main() {
+  const validGameIds: number[] = [];
   const puzzle_input = fs.readFileSync(filePath, { encoding: "utf-8" });
   const lines = puzzle_input.split("\n");
   for (let i = 0; i < lines.length; i++) {
     let line = lines[i];
-    let gameId = line.split(': ')[0].split(' ')[1]
+    let gameId = parseInt(line.split(": ")[0].split(" ")[1]);
     let gameSets = processGameSets(line);
-    console.log(`gameId ${gameId} gameSets ${gameSets}`)
+    let gameValid = true;
+    for (let j = 0; j < gameSets.length; j++) {
+      if (!gameValid) {
+        break;
+      }
+      let gSet: string[] = gameSets[j];
+      for (let k = 0; k < gSet.length; k++) {
+        let draw: string = gSet[k];
+        let drawNum = parseInt(draw.split(" ")[0]);
+        let drawColor = draw.split(" ")[1];
+        if (!validDraw(drawNum, drawColor, possibleCubes)) {
+          gameValid = false;
+          break;
+        }
+      }
+    }
+    if (gameValid) validGameIds.push(gameId);
   }
+  // console.log(`validGameIds: ${validGameIds}`)
+  console.log(sumOfNums(validGameIds));
 }
-//   let numbers: number[] = [];
-//   const puzzle_input = fs.readFileSync(filePath, { encoding: "utf-8" });
-//   const lines = puzzle_input.split("\n");
-//   for (let i = 0; i < lines.length; i++) {
-//     let line = lines[i];
-//     let digitsOnLine = allPossibleDigitsFromLine(line, false);
-
-//     let num = numberFromFirstAndLastDigit(digitsOnLine);
-//     if (!isNaN(num)) {
-//       numbers.push(num);
-//     }
-//   }
-//   // console.log(`numbers ${numbers}`);
-//   const sum = numbers.reduce((accumulator, currentValue) => {
-//     return accumulator + currentValue;
-//   }, 0);
-//   console.log(sum);
-// }
 
 main();
