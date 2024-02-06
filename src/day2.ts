b5f3fc3afed18b7420529fcf8038a88b610d29a5
Mate Rusz <ruszmate33@gmail.com>
2024-02-06 07:07:20 +0100

    day2 part2 in TS solved

--- src/day2.ts
+++ src/day2.ts
@@ -6,6 +6,8 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
+const testInput2 = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green`;
+
 interface colorNumColors {
   [key: string]: number;
 }
@@ -40,20 +42,46 @@ function validDraw(
   return true;
 }
 
+function getMinimumSets(gameSets: string[][]): colorNumColors {
+  let minimumSet: colorNumColors = {};
+  for (let i = 0; i < gameSets.length; i++) {
+    for (let j = 0; j < gameSets[i].length; j++) {
+      let draw = gameSets[i][j];
+      let drawNum = parseInt(draw.split(" ")[0]);
+      let drawColor = draw.split(" ")[1];
+      let currentNumbreForColor = minimumSet[drawColor] ?? 0;
+      if (currentNumbreForColor < drawNum) {
+        minimumSet[drawColor] = drawNum;
+      }
+    }
+  }
+  return minimumSet;
+}
+
 function sumOfNums(nums: number[]): number {
   return nums.reduce((accumulator, currentValue) => {
     return accumulator + currentValue;
   }, 0);
 }
 
+function productOfNums(nums: number[]): number {
+  return nums.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue;
+  }, 1);
+}
+
 function main() {
   const validGameIds: number[] = [];
   const puzzle_input = fs.readFileSync(filePath, { encoding: "utf-8" });
   const lines = puzzle_input.split("\n");
+  let sumOfProductsMinimumSets = 0;
   for (let i = 0; i < lines.length; i++) {
     let line = lines[i];
     let gameId = parseInt(line.split(": ")[0].split(" ")[1]);
     let gameSets = processGameSets(line);
+    sumOfProductsMinimumSets += productOfNums(
+      Object.values(getMinimumSets(gameSets))
+    );
     let gameValid = true;
     for (let j = 0; j < gameSets.length; j++) {
       if (!gameValid) {
@@ -72,8 +100,8 @@ function main() {
     }
     if (gameValid) validGameIds.push(gameId);
   }
-  // console.log(`validGameIds: ${validGameIds}`)
-  console.log(sumOfNums(validGameIds));
+  console.log(`part1: ${sumOfNums(validGameIds)}`);
+  console.log(`part2: ${sumOfProductsMinimumSets}`);
 }
 
 main();
